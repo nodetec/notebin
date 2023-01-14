bd7198cd6846d2c0fe00c41dfc8fa35db341cb88
MAH <69465962+MAHcodes@users.noreply.github.com>
2023-01-14 04:29:19 +0200

    fix: light theme (#7)

--- app/NoteArea.tsx
+++ app/NoteArea.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useTheme } from "next-themes";
 import { useState } from "react";
 import NoteOptions from "./NoteOptions";
 import "@uiw/react-textarea-code-editor/dist.css";
@@ -8,16 +9,18 @@ const CodeEditor = dynamic(
   () => import("@uiw/react-textarea-code-editor").then((mod) => mod.default),
   { ssr: true }
 );
-export default function NoteArea({ user }: any) {
+export default function NoteArea() {
   const [text, setText] = useState("");
   const [syntaxOption, setSyntaxOption] = useState("markdown");
+  const { theme, systemTheme } = useTheme();
+  const isDarkTheme = (theme === "system" ? systemTheme : theme) === "dark";
 
   return (
     <div className="flex flex-row gap-1">
       {/* <div className="w-2/3 h-[20rem] overflow-auto"> */}
       <div className="w-2/3 h-[34rem] overflow-auto">
         <CodeEditor
-          className="w-full min-h-full focus:border focus:border-blue-500 rounded-md p-3 outline-none dark:bg-neutral-800"
+          className="w-full min-h-full focus:border focus:border-blue-500 rounded-md p-3 outline-none"
           value={text}
           language={syntaxOption}
           placeholder="Please enter text."
@@ -25,12 +28,13 @@ export default function NoteArea({ user }: any) {
           padding={25}
           style={{
             fontSize: 15,
+            backgroundColor: `${isDarkTheme ? "rgb(38 38 38)" : "rgb(228 228 231)"}`,
             fontFamily:
               "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
           }}
         />
       </div>
-      <div className="bg-neutral-800 rounded-md w-1/3">
+      <div className="dark:bg-neutral-800 bg-zinc-200 rounded-md w-1/3">
         <NoteOptions text={text} onSetSyntaxOption={setSyntaxOption} />
       </div>
     </div>
