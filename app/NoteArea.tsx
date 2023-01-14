14bb26b664091931fb1bdb02247d12ae46b2f40d
Nicholas Chiarulli <nchiarulli@homesourcesystems.com>
2023-01-13 22:46:41 -0500

    fix: code editor background color and made note area mobile friendly

--- app/NoteArea.tsx
+++ app/NoteArea.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useTheme } from "next-themes";
 import { useState } from "react";
 import NoteOptions from "./NoteOptions";
 import "@uiw/react-textarea-code-editor/dist.css";
@@ -12,13 +11,11 @@ const CodeEditor = dynamic(
 export default function NoteArea() {
   const [text, setText] = useState("");
   const [syntaxOption, setSyntaxOption] = useState("markdown");
-  const { theme, systemTheme } = useTheme();
-  const isDarkTheme = (theme === "system" ? systemTheme : theme) === "dark";
 
   return (
-    <div className="flex flex-row gap-1">
+    <div className="flex flex-col gap-1 lg:flex-row">
       {/* <div className="w-2/3 h-[20rem] overflow-auto"> */}
-      <div className="w-2/3 h-[34rem] overflow-auto">
+      <div className="w-full lg:w-2/3 h-[34rem] overflow-auto">
         <CodeEditor
           className="w-full min-h-full focus:border focus:border-blue-500 rounded-md p-3 outline-none"
           value={text}
@@ -28,13 +25,12 @@ export default function NoteArea() {
           padding={25}
           style={{
             fontSize: 15,
-            backgroundColor: `${isDarkTheme ? "rgb(38 38 38)" : "rgb(228 228 231)"}`,
             fontFamily:
               "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
           }}
         />
       </div>
-      <div className="dark:bg-neutral-800 bg-zinc-200 rounded-md w-1/3">
+      <div className="dark:bg-neutral-800 bg-zinc-200 rounded-md w-full lg:w-1/3">
         <NoteOptions text={text} onSetSyntaxOption={setSyntaxOption} />
       </div>
     </div>
