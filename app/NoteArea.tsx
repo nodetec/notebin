97f1c14406f8863663ae881422af44c90ebbc727
mahcodes <mhmdali102@bk.ru>
2023-01-15 19:59:26 +0200

    feat: implement new disgin functionality

--- app/NoteArea.tsx
+++ app/NoteArea.tsx
@@ -1,19 +1,80 @@
 "use client";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "@uiw/react-textarea-code-editor/dist.css";
 import dynamic from "next/dynamic";
-import { LANGUAGES } from "./constants";
+import { LANGUAGES, RELAYS } from "./constants";
 import Button from "./Button";
+import { RelayContext } from "./context/relay-provider";
+import { NostrService } from "./utils/NostrService";
+import { EventContext } from "./context/event-provider";
+import { useRouter } from "next/navigation";
 
 const CodeEditor = dynamic(
   () => import("@uiw/react-textarea-code-editor").then((mod) => mod.default),
   { ssr: true }
 );
-export default function NoteArea({ onSetSyntaxOption }: any) {
+
+const NoteArea = () => {
   const [text, setText] = useState("");
   const [syntax, setSyntax] = useState("markdown");
-  const [syntaxOption, setSyntaxOption] = useState("markdown");
+  const [syntaxOption] = useState("markdown");
   const [postLoading, setPostLoading] = useState(false);
+  // @ts-ignore
+  const { setEvent } = useContext(EventContext);
+  // @ts-ignore
+  const { relay, setRelay } = useContext(RelayContext);
+  const router = useRouter();
+  const [tagsList, setTagsList] = useState<string[]>([]);
+  const [tipInfo, setTipInfo] = useState({
+    noteAddress: "",
+    customValue: "",
+  });
+
+  const post = async (e: any) => {
+    e.preventDefault();
+    setPostLoading(true);
+
+    let localRelay = relay;
+
+    if (!localRelay) {
+      localRelay = await NostrService.connect(RELAYS[0]);
+      setRelay(localRelay);
+    }
+
+    // const privateKey = null;
+    // const publicKey = null;
+    // @ts-ignore
+    const publicKey = await nostr.getPublicKey();
+    let event = NostrService.createEvent(
+      publicKey,
+      text,
+      syntax,
+      tipInfo.noteAddress,
+      tipInfo.customValue,
+      tagsList
+    );
+    event = await NostrService.addEventData(event);
+
+    let pub = localRelay.publish(event);
+    pub.on("ok", () => {
+      console.debug(`${localRelay.url} has accepted our event`);
+    });
+
+    pub.on("seen", async () => {
+      console.debug(`we saw the event on ${localRelay.url}`);
+      // @ts-ignore
+      const retrieved_event = await NostrService.getEvent(event.id, localRelay);
+      console.log("got event:", retrieved_event);
+      await setEvent(retrieved_event);
+      console.log("did it");
+      router.push("/note/" + event.id);
+    });
+
+    pub.on("failed", (reason: any) => {
+      setPostLoading(false);
+      console.error(`failed to publish to ${localRelay.url}: ${reason}`);
+    });
+  };
 
   return (
     <div className="w-full lg:w-2/3 mx-auto">
@@ -22,7 +83,10 @@ export default function NoteArea({ onSetSyntaxOption }: any) {
           <input className="bg-zinc-200 text-neutral-900 dark:bg-neutral-900 dark:text-zinc-300 border-0 outline-0 focus:ring-0 text-sm rounded-md"
             type="text"
             list="syntax-languages"
-            placeholder="syntax" />
+            placeholder="syntax"
+            value={syntax}
+            onChange={(e) => setSyntax(e.target.value)}
+          />
           <datalist id="syntax-languages">
             {LANGUAGES.map((lang) => (
               <option key={lang} value={lang}>{lang}</option>
@@ -51,6 +115,7 @@ export default function NoteArea({ onSetSyntaxOption }: any) {
           size="sm"
           className="w-auto ml-auto"
           loading={postLoading}
+          onClick={post}
         >
           {postLoading ? "Sending..." : "Create Note"}
         </Button>
@@ -58,3 +123,5 @@ export default function NoteArea({ onSetSyntaxOption }: any) {
     </div>
   );
 }
+
+export default NoteArea;
