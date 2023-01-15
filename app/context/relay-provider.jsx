c814998219c499ca27e2853a524b4103a7c5b720
Christian Chiarulli <chris.machine@pm.me>
2023-01-15 03:56:42 -0500

    feat: improved note page (#14)

--- app/context/relay-provider.jsx
+++ app/context/relay-provider.jsx
@@ -5,7 +5,7 @@ import { createContext, useState } from "react";
 export const RelayContext = createContext(null);
 
 export default function RelayProvider({ children }) {
-  const [relay, setRelay] = useState(null);
+  let [relay, setRelay] = useState(null);
 
   return (
     <RelayContext.Provider value={{ relay, setRelay }}>
