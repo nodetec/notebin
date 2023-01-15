845cbe9a47e1e2af41af442929aa69b0240fce87
Nicholas Chiarulli <31137507+nicholaschiarulli@users.noreply.github.com>
2023-01-15 15:41:34 -0500

    feat: display created note on note page in read only manner (#16)

--- app/NoteArea.tsx
+++ app/NoteArea.tsx
@@ -77,7 +77,7 @@ const NoteArea = () => {
 
   return (
     <div className="w-full lg:w-2/3 mx-auto">
-      <div className="overflow-auto rounded-md border-2 border-zinc-400 dark:border-neutral-700 ">
+      <div className="rounded-md border-2 border-zinc-400 dark:border-neutral-700">
         <div className="bg-zinc-300 dark:bg-neutral-800 p-2">
           <input className="bg-zinc-200 text-neutral-900 dark:bg-neutral-900 dark:text-zinc-300 border-0 outline-0 focus:ring-0 text-sm rounded-md"
             type="text"
@@ -92,21 +92,24 @@ const NoteArea = () => {
             ))}
           </datalist>
         </div>
-        <CodeEditor
-          className="w-full min-h-full focus:border focus:border-blue-500 p-3 outline-none h-[34rem]"
-          value={text}
-          language={syntax}
-          placeholder="Enter your note..."
-          autoCapitalize="none"
-          onChange={(evn) => setText(evn.target.value)}
-          padding={25}
-          style={{
-            fontSize: 15,
-            fontFamily:
-              "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
-          }}
-        />
+        <div className="overflow-auto h-[34rem]">
+          <CodeEditor
+            className="w-full focus:border focus:border-blue-500 p-3 outline-none"
+            value={text}
+            language={syntax}
+            placeholder="Enter your note..."
+            autoCapitalize="none"
+            onChange={(evn) => setText(evn.target.value)}
+            padding={25}
+            style={{
+              fontSize: 15,
+              fontFamily:
+                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+            }}
+          />
+        </div>
       </div>
+
       <div className="pt-2">
         <Button
           color="blue"
--- app/note/[eventid]/page.tsx
+++ app/note/[eventid]/page.tsx
@@ -2,11 +2,18 @@
 
 import { EventContext } from "../../context/event-provider.jsx";
 import { useContext, useEffect, useState } from "react";
+import "@uiw/react-textarea-code-editor/dist.css";
+import dynamic from "next/dynamic";
 import Button from "../../Button";
 import { BsLightningChargeFill } from "react-icons/bs";
 import { usePathname } from "next/navigation.js";
 import { RelayContext } from "../../context/relay-provider.jsx";
 import { NostrService } from "../../utils/NostrService";
+
+const CodeEditor = dynamic(
+  () => import("@uiw/react-textarea-code-editor").then((mod) => mod.default),
+  { ssr: true }
+);
 
 export default function NotePage({ params }: any) {
   // @ts-ignore
@@ -60,17 +67,42 @@ export default function NotePage({ params }: any) {
     <div>
       <div className="flex flex-col gap-4 justify-start">
         <h1 className="text-slate-400 text-2xl">Event ID: {event?.id}</h1>
-        <p className="p-4 text-neutral-300 bg-neutral-800 rounded-md">
-          {event?.content}
-        </p>
-        <Button
-          color="yellow"
-          onClick={handleTip}
-          size="sm"
-          icon={<BsLightningChargeFill size="14" />}
-        >
-          tip
-        </Button>
+        <div className="rounded-md border-2 border-zinc-400 dark:border-neutral-700 ">
+          <div className="flex justify-between bg-zinc-300 dark:bg-neutral-800 p-2">
+            <input
+              className="bg-zinc-200 text-neutral-900 dark:bg-neutral-900 dark:text-zinc-300 border-0 outline-0 focus:ring-0 text-sm rounded-md"
+              type="text"
+              list="syntax-languages"
+              placeholder="syntax"
+              disabled
+              value={event?.tags[0][1]}
+            />
+            <Button
+              color="yellow"
+              onClick={handleTip}
+              size="sm"
+              icon={<BsLightningChargeFill size="14" />}
+            >
+              tip
+            </Button>
+          </div>
+          <div className="overflow-auto h-[34rem]">
+            <CodeEditor
+              className="w-full focus:border focus:border-blue-500 p-3 outline-none"
+              value={event?.content}
+              language={event?.tags[0][1]}
+              placeholder="Enter your note..."
+              autoCapitalize="none"
+              padding={25}
+              disabled
+              style={{
+                fontSize: 15,
+                fontFamily:
+                  "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+              }}
+            />
+          </div>
+        </div>
       </div>
       {/* <p className="text-slate-600">pubkey: {event?.pubkey}</p> */}
       {/* <p className="text-slate-600">kind: {event?.kind}</p> */}
