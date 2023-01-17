0648083c19f33abaf5a88aab11dff18876433ea1
Christian Chiarulli <chris.machine@pm.me>
2023-01-16 23:29:31 -0500

    feat: user profile

--- app/profile/[profileid]/Profile.tsx
+++ app/profile/[profileid]/Profile.tsx
@@ -19,13 +19,25 @@ const Profile = ({ pubkey }: any) => {
 
   console.log("profile data", data);
 
+  const shortenHash = (hash: string | undefined) => {
+    if (hash) {
+      return (
+        " " + hash.substring(0, 4) + "..." + hash.substring(hash.length - 4)
+      );
+    }
+  };
+
   return (
-    <>
-      <p>Name: {data?.name}</p>
-      <p>Public key: {data?.npub}</p>
-      <p>About: {data?.about}</p>
-      <img src={data?.picture} />
-    </>
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-row items-center gap-4">
+        <img className="rounded-full w-28" src={data?.picture} />
+        <p className="text-4xl font-bold pt-4 text-zinc-200">@{data?.name}</p>
+      </div>
+      <div className="border border-zinc-500 rounded-md p-4">
+        <p className="text-lg text-zinc-400">{shortenHash(data?.npub)}</p>
+        <p className="text-sm text-zinc-400">{data?.about}</p>
+      </div>
+    </div>
   );
 };
 
