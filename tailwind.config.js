0b0c38ebf64ba25adc803cc52e6641c767b08348
Christian Chiarulli <chris.machine@pm.me>
2023-01-17 23:14:32 -0500

    feat: tailwind config theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      scale: {
+        101: "1.01",
+      },
       boxShadow: {
         overlay: "0 0 0 100vmax rgba(0, 0, 0, 0.7)",
       },
