39b37e65a3005b09b81d9bb3811e72dd173227a6
Christian Chiarulli <chris.machine@pm.me>
2023-01-19 02:11:44 -0500

    refactor: header is server side rendered

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,74 +1,9 @@
-"use client";
 import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
-import { BsLightningChargeFill } from "react-icons/bs";
 import { TbNote } from "react-icons/tb";
-import Popup from "./Popup";
-
-import Button from "./Button";
-import AccountButton from "./AccountButton";
-
-import { KeysContext } from "./context/keys-provider.jsx";
 import ConnectedRelaysStatus from "./ConnectedRelaysStatus";
+import Login from "./Login";
 
 export default function Header() {
-  // @ts-ignore
-  const { keys, setKeys } = useContext(KeysContext);
-  const [isOpen, setIsOpen] = useState(false);
-  const [isLightningConnected, setIsLightningConnected] = useState(false);
-
-  useEffect(() => {
-    const shouldReconnect = localStorage.getItem("shouldReconnect");
-
-    const getConnected = async (shouldReconnect: string) => {
-      let enabled = false;
-      // @ts-ignore
-      if (shouldReconnect === "true" && !webln.executing) {
-        try {
-          // @ts-ignore
-          enabled = await window.webln.enable();
-          setIsLightningConnected(true);
-          // @ts-ignore
-          const publicKey = await nostr.getPublicKey();
-          console.log("public key", publicKey);
-          setKeys({ privateKey: "", publicKey: publicKey });
-        } catch (e: any) {
-          console.log(e.message);
-        }
-      }
-      return enabled;
-    };
-
-    if (shouldReconnect === "true") {
-      getConnected(shouldReconnect);
-    }
-  }, []);
-
-  const handleClick = async () => {
-    setIsOpen(true);
-  };
-
-  const connectLightningHandler = async () => {
-    // @ts-ignore
-    if (typeof window.webln !== "undefined") {
-      // @ts-ignore
-      await window.webln.enable();
-      // @ts-ignore
-      const publicKey = await nostr.getPublicKey();
-      setKeys({ private: "", publicKey: publicKey });
-      localStorage.setItem("shouldReconnect", "true");
-    }
-    console.log("connected to lightning");
-    setIsLightningConnected(true);
-    setIsOpen(false);
-  };
-
-  /* const shortenHash = (hash: string) => {
-    if (hash) {
-      return hash.substring(0, 4) + "..." + hash.substring(hash.length - 4);
-    }
-  }; */
-
   return (
     <div>
       <nav className="flex justify-between flex-row items-stretch pb-12 gap-4">
@@ -92,32 +27,10 @@ export default function Header() {
           </div>
           <div className="flex gap-4">
             <ConnectedRelaysStatus />
-            {isLightningConnected && keys?.publicKey ? (
-              <AccountButton publicKey={keys?.publicKey} />
-            ) : (
-              <Button
-                color="zincDark"
-                variant="outline"
-                onClick={handleClick}
-                size="sm"
-              >
-                login
-              </Button>
-            )}
+            <Login />
           </div>
         </div>
       </nav>
-      <Popup title="Generate Keys" isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Button
-          className="w-full"
-          onClick={connectLightningHandler}
-          color="yellow"
-          size="sm"
-          icon={<BsLightningChargeFill size="14" />}
-        >
-          {isLightningConnected ? "connected" : "Login with NIP-07 Extension"}
-        </Button>
-      </Popup>
     </div>
   );
 }
