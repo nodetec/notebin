8656f7a07999a8909c0769d8626506e7beb2e992
Jason Chiarulli <jchiarulli@pm.me>
2023-01-19 17:11:17 -0500

    feat: added open graph and twitter meta tags (#84)

--- app/head.tsx
+++ app/head.tsx
@@ -2,9 +2,31 @@ export default function Head() {
   return (
     <>
       <title>notebin</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Decentralized notes over relay" />
+      <meta name="charset" content="utf-8" />
+      <meta name="viewport"content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Decentralized notes using Nostr" />
+      <meta name="author" content="notebin devs" />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:title" content="notebin" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/images/logos/notebin-card.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:alt" content="notebin logo" />
+      <meta property="og:url" content="https://www.notebin.org/" />
+      <meta property="og:description" content="Decentralized notes using Nostr" />
+      <meta property="og:site_name" content="notebin" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content="notebin" />
+      <meta property="twitter:description" content="Decentralized notes using Nostr" />
+      <meta property="twitter:url" content="https://www.notebin.org/" />
+      <meta property="twitter:site" content="@chrisatmachine" />
+      <meta property="twitter:image" content="/images/logos/notebin-card.png" />
+      <meta property="twitter:image:alt" content="notebin logo" />
+      <meta property="twitter:creator" content="@chrisatmachine" />
     </>
   );
 }
+
