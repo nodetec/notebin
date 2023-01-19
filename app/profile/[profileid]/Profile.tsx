0be7c457b64f6ba63aff26ed322116d9cbe98275
mahcodes <mhmdali102@bk.ru>
2023-01-19 23:29:29 +0200

    profilecard mvp

--- app/profile/[profileid]/Profile.tsx
+++ app/profile/[profileid]/Profile.tsx
@@ -50,8 +50,8 @@ export default function Profile({ pubkey }: any) {
   // nip06?: string | undefined;
 
   return (
-    <div className="flex flex-col sm:flex-row gap-8 flex-1">
-      <div className="flex flex-col flex-shrink">
+    <div className="flex flex-col md:flex-row items-start gap-8 flex-1">
+      <div className="flex flex-col flex-shrink md:sticky top-4">
         <UserCard
           name={name}
           npub={shortenHash(npub)}
--- app/profile/[profileid]/UserCard.tsx
+++ app/profile/[profileid]/UserCard.tsx
@@ -1,15 +1,13 @@
 export default function UserCard({ name, npub, about, picture }: any) {
   return (
-    <div className="border border-tertiary rounded-md p-4 flex flex-row justify-between items-center gap-4">
-      <div>
-        <p className="text-4xl font-bold pt-4 text-zinc-200">
-          <span className="text-red-500">@</span>
-          {name}
-        </p>
-        <p className="text-lg text-accent">{npub}</p>
-        <p className="text-sm text-accent">{about}</p>
-      </div>
-      <img className="rounded-full w-28" src={picture} />
+    <div className="p-4 flex flex-col items-start gap-4">
+      <img className="rounded-full w-32" src={picture} alt={name} />
+      <p className="text-2xl font-bold text-zinc-200">
+        <span className="text-red-500">@</span>
+        {name}
+      </p>
+      <p className="text-lg text-accent">{npub}</p>
+      <p className="text-sm text-accent">{about}</p>
     </div>
   );
 }
