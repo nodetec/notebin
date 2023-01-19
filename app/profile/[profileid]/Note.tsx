71e2e92abe52d135bcc4145c004780f81621f924
mahcodes <mhmdali102@bk.ru>
2023-01-19 23:49:10 +0200

    refactor: note

--- app/profile/[profileid]/Note.tsx
+++ app/profile/[profileid]/Note.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { DetailedHTMLProps, FC, LiHTMLAttributes } from "react";
-import { BsFillTagFill } from "react-icons/bs";
+import { DetailedHTMLProps, FC, LiHTMLAttributes, ReactNode } from "react";
+import { BsFillFileEarmarkCodeFill, BsFillTagFill } from "react-icons/bs";
 import { MdDateRange } from "react-icons/md";
 
 interface NoteProps
@@ -20,13 +20,42 @@ const Note: FC<NoteProps> = ({
 }) => {
   const getValues = (name: string) => {
     const [itemTag] = tags.filter((tag: string[]) => tag[0] === name);
-    const [,item] = itemTag || [, undefined];
+    const [, item] = itemTag || [, undefined];
     return item;
-  }
+  };
 
   const actualTags = getValues("tags").split(",");
   const title = getValues("subject");
 
+  return (
+    <li
+      className="rounded-md hover:shadow-sm hover:scale-101 transition-transform hover:shadow-accent dark:bg-secondary text-accent"
+      {...props}
+    >
+      <Link href={`/note/${noteId}`} className="p-5 flex flex-col gap-3">
+        {title ? (
+          <h3 className="text-2xl font-semibold text-light twolines">
+            {title}
+          </h3>
+        ) : null}
+        <div className="flex items-center gap-5 opacity-70">
+          <DatePosted timestamp={createdAt} />
+          <FileType type={getValues("filetype")} />
+          {actualTags.length > 1 ? <NoteTags tags={actualTags} /> : null}
+        </div>
+        <div className="twolines opacity-70">{content}</div>
+      </Link>
+    </li>
+  );
+};
+
+const InfoContainer = ({ children }: { children: ReactNode }) => (
+  <div className="flex items-center gap-2">
+    {children}
+  </div>
+)
+
+const DatePosted = ({ timestamp }: { timestamp: number }) => {
   const timeStampToDate = (timestamp: number) => {
     let date = new Date(timestamp * 1000);
     return date.toLocaleDateString("en-US", {
@@ -37,36 +66,37 @@ const Note: FC<NoteProps> = ({
   };
 
   return (
-    <li
-      className="rounded-md hover:shadow-sm hover:scale-101 transition-transform hover:shadow-accent dark:bg-secondary text-accent"
-      {...props}
-    >
-      <Link href={`/note/${noteId}`} className="p-5 flex flex-col gap-3">
-        {title ? <h3 className="text-2xl font-semibold text-light twolines">{title}</h3> : null}
-        <div className="flex items-center gap-5 opacity-70">
-          <span className="text-sm flex items-center gap-2">
-            <span>
-              <MdDateRange className="w-4 h-4 text-current" />
-            </span>
-            {timeStampToDate(createdAt)}
-          </span>
-          {actualTags.length > 1 ? (
-            <div className="flex items-center gap-2">
-              <span>
-                <BsFillTagFill className="w-4 h-4 text-current" />
-              </span>
-              <ul className="flex items-center gap-2">
-                {actualTags.map((tag: string) => (
-                  <li key={tag}>#{tag}</li>
-                ))}
-              </ul>
-            </div>
-          ) : null}
-        </div>
-        <div className="twolines opacity-70">{content}</div>
-      </Link>
-    </li>
+    <InfoContainer>
+      <span>
+        <MdDateRange className="w-4 h-4 text-current" />
+      </span>
+      <span>
+        {timeStampToDate(timestamp)}
+      </span>
+    </InfoContainer>
   );
 };
+
+const NoteTags = ({tags}: {tags: string[]}) => (
+  <InfoContainer>
+    <span>
+      <BsFillTagFill className="w-4 h-4 text-current" />
+    </span>
+    <ul className="flex items-center gap-2">
+      {tags.map((tag: string) => (
+        <li key={tag}>#{tag}</li>
+      ))}
+    </ul>
+  </InfoContainer>
+)
+
+const FileType = ({ type }: { type: string }) => (
+  <InfoContainer>
+    <span>
+      <BsFillFileEarmarkCodeFill className="w-4 h-4 text-current" />
+    </span>
+    <span>{type}</span>
+  </InfoContainer>
+)
 
 export default Note;
