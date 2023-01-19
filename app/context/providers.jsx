fb083ffdd2e86a3f43f51371673fa6768895156c
Christian Chiarulli <chris.machine@pm.me>
2023-01-19 04:45:05 -0500

    feat: use cached event when creating post

--- app/context/providers.jsx
+++ app/context/providers.jsx
@@ -3,20 +3,17 @@
 import EventProvider from "./event-provider.jsx";
 import KeysProvider from "./keys-provider.jsx";
 import { CustomThemeProvider } from "./theme-provider";
-import RelayProvider from "./relay-provider.jsx";
 import { NostrProvider } from "nostr-react";
 import { RELAYS } from "../utils/constants";
 
 export default function Providers({ children }) {
   return (
-    <NostrProvider relayUrls={RELAYS} debug={false}>
-      <RelayProvider>
-        <EventProvider>
-          <KeysProvider>
-            <CustomThemeProvider>{children}</CustomThemeProvider>
-          </KeysProvider>
-        </EventProvider>
-      </RelayProvider>
+    <NostrProvider relayUrls={RELAYS} debug={true}>
+      <EventProvider>
+        <KeysProvider>
+          <CustomThemeProvider>{children}</CustomThemeProvider>
+        </KeysProvider>
+      </EventProvider>
     </NostrProvider>
   );
 }
--- app/note/[eventid]/page.tsx
+++ app/note/[eventid]/page.tsx
@@ -3,12 +3,22 @@ import { usePathname } from "next/navigation";
 import { useNostrEvents } from "nostr-react";
 import Note from "./Note";
 import { Event } from "nostr-tools";
+import { EventContext } from "../../context/event-provider";
+import { useContext } from "react";
 
 export default function NotePage() {
   const pathname = usePathname();
   let eventId = "";
   if (pathname) {
     eventId = pathname.split("/").pop() || "";
+  }
+
+  // @ts-ignore
+  const { event: cachedEvent } = useContext(EventContext);
+
+  if (cachedEvent && eventId === cachedEvent.id) {
+    console.log("using cached event");
+    return <Note event={cachedEvent} />;
   }
 
   const { events } = useNostrEvents({
