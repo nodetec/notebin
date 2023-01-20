a996b94c70b81727d9d96221af3b066089c93f81
Jay the Code Monkey <jchiarulli@pm.me>
2023-01-19 19:36:13 -0500

    fix: updated the open graph and twitter images and the favicons to match the background color in the redesign

--- app/head.tsx
+++ app/head.tsx
@@ -13,13 +13,13 @@ export default function Head() {
       <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=0" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=0" />
       <link rel="manifest" href="/site.webmanifest?v=0" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=0" color="#171717" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=0" color="#121a23" />
       <link rel="shortcut icon" href="/favicon.ico?v=0" />
       <meta name="apple-mobile-web-app-title" content="notebin" />
       <meta name="application-name" content="notebin" />
-      <meta name="msapplication-TileColor" content="#171717" />
+      <meta name="msapplication-TileColor" content="#121a23" />
       <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=0" />
-      <meta name="theme-color" content="#171717" />
+      <meta name="theme-color" content="#121a23" />
 
       <meta property="og:title" content="notebin" />
       <meta property="og:type" content="website" />
