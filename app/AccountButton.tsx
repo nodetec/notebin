646e299cd624f6416e18b7ae4cb53117e31f3190
mahcodes <mhmdali102@bk.ru>
2023-01-20 09:43:30 +0200

    feat: dummy photos for when user doesn't have photo

--- app/AccountButton.tsx
+++ app/AccountButton.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useProfile } from "nostr-react";
 import { shortenHash } from "./lib/utils";
+import { DUMMY_PROFILE_API } from "./utils/constants";
 
 interface AccountButtonProps {
   pubkey: string;
@@ -14,9 +15,7 @@ export default function AccountButton({ pubkey }: AccountButtonProps) {
   return (
     <Link href={`/u/` + pubkey}>
       <span className="flex gap-2 dark:bg-primary text-accent border border-tertiary rounded-full py-2 px-3 hover:border-current">
-        {data?.picture && (
-          <img className="rounded-full w-6" src={data?.picture} />
-        )}
+        <img className="rounded-full w-6" src={data?.picture||DUMMY_PROFILE_API(data?.name||data?.npub!)} />
         {shortenHash(pubkey)}
       </span>
     </Link>
