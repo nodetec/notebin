f76abdbf907a15529e2253288b28a8b01e0cf3f6
Jason Chiarulli <jchiarulli@pm.me>
2023-01-19 19:37:48 -0500

    feat: added favicons for browsers, apple, android, safari, and window… (#87)
    * feat: added favicons for browsers, apple, android, safari, and windows and a webmanifest
    
    * fix: updated the open graph and twitter images and the favicons to match the background color in the redesign

--- app/head.tsx
+++ app/head.tsx
@@ -6,7 +6,20 @@ export default function Head() {
       <meta name="viewport"content="width=device-width, initial-scale=1" />
       <meta name="description" content="Decentralized notes using Nostr" />
       <meta name="author" content="notebin devs" />
-      <link rel="icon" href="/favicon.ico" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=0" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=0" />
+      <link rel="icon" type="image/png" sizes="194x194" href="/favicon-194x194.png?v=0" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png?v=0" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=0" />
+      <link rel="manifest" href="/site.webmanifest?v=0" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg?v=0" color="#121a23" />
+      <link rel="shortcut icon" href="/favicon.ico?v=0" />
+      <meta name="apple-mobile-web-app-title" content="notebin" />
+      <meta name="application-name" content="notebin" />
+      <meta name="msapplication-TileColor" content="#121a23" />
+      <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=0" />
+      <meta name="theme-color" content="#121a23" />
 
       <meta property="og:title" content="notebin" />
       <meta property="og:type" content="website" />
