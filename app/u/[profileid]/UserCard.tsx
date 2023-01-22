befe6c341c37f7780ca1688945ce82b3848b467d
Nicholas Chiarulli <31137507+nicholaschiarulli@users.noreply.github.com>
2023-01-22 01:53:46 -0500

    feat: added edit profile popover (#109)

--- app/u/[profileid]/UserCard.tsx
+++ app/u/[profileid]/UserCard.tsx
@@ -1,6 +1,10 @@
 import Buttons from "../../Buttons";
 import FollowButton from "./FollowButton";
 import Truncate from "../../Truncate";
+import Popup from "../../Popup";
+import PopupInput from "../../PopupInput";
+import { useState } from "react";
+import Button from "../../Button";
 
 export default function UserCard({
   name,
@@ -12,7 +16,10 @@ export default function UserCard({
   loggedInUserPublicKey,
 }: any) {
   const contacts = loggedInUsersContacts.map((pair: string) => pair[1]);
-
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = async () => {
+    setIsOpen(!isOpen);
+  };
   /* (contacts.includes(pubkey) */
 
   return (
@@ -30,7 +37,18 @@ export default function UserCard({
         <Truncate content={npub} color="transparent" size="sm" />
       </p>
       <p className="text-sm text-accent">{about}</p>
-      {loggedInUserPublicKey === pubkey ? null : (
+      {loggedInUserPublicKey === pubkey ? (
+        <Buttons>
+          <Button
+            color="blue"
+            variant="outline"
+            onClick={handleClick}
+            size="sm"
+          >
+            edit profile
+          </Button>
+        </Buttons>
+      ) : (
         <Buttons>
           <FollowButton
             loggedInUserPublicKey={loggedInUserPublicKey}
@@ -40,6 +58,22 @@ export default function UserCard({
           />
         </Buttons>
       )}
+      <Popup title="Edit Profile" isOpen={isOpen} setIsOpen={setIsOpen}>
+        <PopupInput label="Profile Image Url"></PopupInput>
+        <PopupInput label="Name*"></PopupInput>
+        <PopupInput label="Bio"></PopupInput>
+        <PopupInput label="Node Address"></PopupInput>
+        <PopupInput label="Custom Record (if applicable)"></PopupInput>
+        <Button
+          color="blue"
+          variant="solid"
+          onClick={handleClick}
+          size="sm"
+          className="w-1/4"
+        >
+          Save
+        </Button>
+      </Popup>
     </div>
   );
 }
