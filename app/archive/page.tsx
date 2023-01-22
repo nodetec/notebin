0d3127d4452510e7f13d7b089759edf0f37f4744
ChristianChiarulli <ChristianChiarulli@users.noreply.github.com>
2023-01-22 03:39:28 +0000

    Prettified Code!

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -48,7 +48,12 @@ export default function ArchivePage() {
   return (
     <div className="flex flex-col justify-center gap-3">
       <h1 className="text-3xl">Note Archive</h1>
-      <ArchiveNotes postPerPage={POSTS_PER_PAGE} events={events} numPages={numPages} setFilter={setFilter} />
+      <ArchiveNotes
+        postPerPage={POSTS_PER_PAGE}
+        events={events}
+        numPages={numPages}
+        setFilter={setFilter}
+      />
     </div>
   );
 }
