8f0204fadd435fc285420274bedf0b6f32ebf1f9
MAH <69465962+MAHcodes@users.noreply.github.com>
2023-01-25 22:51:49 +0200

    feat: pagination for user notes (#142)

--- app/u/[npub]/LatestNotes.tsx
+++ app/u/[npub]/LatestNotes.tsx
@@ -1,15 +1,35 @@
+import { useSearchParams } from "next/navigation";
 import { useNostrEvents } from "nostr-react";
+import { useEffect, useMemo, useState } from "react";
+import Pagination from "../../Pagination";
 import Posts from "../../Posts";
 import Card from "./Card";
+
+const POSTS_PER_PAGE = 5;
 
 export default function LatestNotes({ profilePubkey, name }: any) {
   const { events } = useNostrEvents({
     filter: {
       kinds: [2222],
       authors: [profilePubkey],
-      limit: 5,
     },
   });
+
+  const [numPages, setNumPages] = useState<number>(0);
+  const searchParams = useSearchParams();
+  const pageSearchParam = searchParams.get("page");
+  const currentPage = pageSearchParam ? parseInt(pageSearchParam) : 1;
+
+  useEffect(() => {
+    setNumPages(Math.ceil(events.length / POSTS_PER_PAGE));
+  }, [events]);
+
+  const slicedEvents = useMemo(() => {
+    return events.slice(
+      currentPage * POSTS_PER_PAGE - POSTS_PER_PAGE,
+      currentPage * POSTS_PER_PAGE
+    );
+  }, [events, currentPage]);
 
   return (
     <Posts
@@ -21,10 +41,11 @@ export default function LatestNotes({ profilePubkey, name }: any) {
       noPosts={events.length === 0}
     >
       <ul className="flex flex-col gap-4 text-center md:text-start">
-        {events.map((event) => (
+        {slicedEvents.map((event) => (
           <Card key={event.id} event={event} dateOnly />
         ))}
       </ul>
+      <Pagination numPages={numPages} />
     </Posts>
   );
 }
