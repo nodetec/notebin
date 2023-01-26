e4619fd0036f0e9168058d22d3d98be49ec5a3e8
mahcodes <mhmdali102@bk.ru>
2023-01-26 03:48:36 +0200

    feat: added back button for posts

--- app/Posts.tsx
+++ app/Posts.tsx
@@ -1,4 +1,7 @@
-import { HTMLAttributes, ReactNode, useContext } from "react";
+import { useRouter } from "next/navigation";
+import { HTMLAttributes, ReactNode } from "react";
+import { IoChevronBack } from "react-icons/io5";
+import Button from "./Button";
 
 interface PostsProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
@@ -6,13 +9,20 @@ interface PostsProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Posts = ({ title, children, className, ...props }: PostsProps) => {
+  const router = useRouter();
   return (
     <div
-      className={`flex flex-col justify-center gap-3 px-2 w-full max-w-[50rem] mx-auto ${className}`}
+      className={`flex flex-col justify-center flex-1 gap-3 px-2 w-full max-w-[50rem] mx-auto ${className}`}
       {...props}
     >
       <div className="flex items-center justify-between gap-4">
-        <h1 className="text-3xl">{title}</h1>
+        <Button
+          color="neutralLight"
+          variant="ghost"
+          icon={<IoChevronBack />}
+          onClick={() => router.back()}
+        />
+        <h1 className="text-3xl flex-1">{title}</h1>
       </div>
       {children}
     </div>
