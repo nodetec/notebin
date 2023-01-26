961ecd830aa227f8f17a8e1421e477abf24a819c
mahcodes <mhmdali102@bk.ru>
2023-01-26 02:35:54 +0200

    fix: footer overlapping popup

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -57,7 +57,7 @@ const Footer = () => (
             href={social.url}
             target="_blank"
             rel="nofollow noopener noreferrer"
-            className="flex items-center gap-2 text-accent opacity-70 hover:opacity-100 text-center"
+            className="flex items-center gap-2 text-accent text-center"
           >
             <span>
               <social.Icon />
