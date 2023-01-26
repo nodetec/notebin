2d3e78a7b795bc215a4ccd03ead0920c4893bc62
MAHcodes <MAHcodes@users.noreply.github.com>
2023-01-26 21:16:18 +0000

    Prettified Code!

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -6,7 +6,7 @@ interface LoadingProps {
   className?: string;
 }
 
-const Loading:FC<LoadingProps> = ({className = "", ...props}) => (
+const Loading: FC<LoadingProps> = ({ className = "", ...props }) => (
   <div className="flex items-center justify-center h-full flex-1">
     <ImSpinner9 className={`animate-spin ${className}`} size="24" {...props} />
   </div>
