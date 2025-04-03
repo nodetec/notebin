afeb7c55b100db32e4e34adef101c1dfa7e90906
Christian Chiarulli <chris.machine@pm.me>
2025-04-03 01:43:27 -0400

    feat: improve pagination

--- src/features/navigation/components/ArchiveNavButton.tsx
+++ src/features/navigation/components/ArchiveNavButton.tsx
@@ -1,14 +1,30 @@
+"use client";
+
 import { ArchiveIcon } from "lucide-react";
 import { Button } from "~/components/ui/button";
-import Link from "next/link";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useAppState } from "~/store";
 
 export function ArchiveNavButton() {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const setPageHistory = useAppState((state) => state.setPageHistory);
+  const setCurrentPageIndex = useAppState((state) => state.setCurrentPageIndex);
+
+  const handleClick = async () => {
+    setPageHistory([]);
+    setCurrentPageIndex(0);
+    await queryClient.invalidateQueries({
+      queryKey: ["snippets"],
+    });
+    router.push("/archive");
+  };
+
   return (
-    <Link href="/archive">
-      <Button className="font-mono" variant="outline">
-        <ArchiveIcon />
-        <span className="hidden md:block">Archive</span>
-      </Button>
-    </Link>
+    <Button className="font-mono" variant="outline" onClick={handleClick}>
+      <ArchiveIcon />
+      <span className="hidden md:block">Archive</span>
+    </Button>
   );
 }
