e7e3c4df6dca4d430eae920e16a16d9def9309ac
Cristy Almonte <44328476+cristyalmonte@users.noreply.github.com>
2025-06-29 16:46:18 -0400

    chore: add mcp test script

---
+++ scripts/test-mcp.mjs
@@ -0,0 +1,33 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
+
+const origin = process.argv[2] || "https://notebin.io";
+
+async function main() {
+  const transport = new SSEClientTransport(new URL(`${origin}/sse`));
+
+  const client = new Client(
+    {
+      name: "example-client",
+      version: "1.0.0",
+    },
+    {
+      capabilities: {
+        prompts: {},
+        resources: {},
+        tools: {},
+      },
+    }
+  );
+
+  console.log("Connecting to", origin);
+  await client.connect(transport);
+
+  console.log("Connected", client.getServerCapabilities());
+
+  const result = await client.listTools();
+  console.log(result);
+  client.close();
+}
+
+main();
